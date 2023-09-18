00922816dd6c9c2b3c2c138a0c23afebd7b7c582
MayraRobles <mlrobles@miners.utep.edu>
2023-09-18 16:01:23 -0600

    Replaced font with 8x11

--- draw_chars.c
+++ draw_chars.c
@@ -17,9 +17,9 @@ void print_char_11x16(char c)
 void print_char_8x12(char c)
 {
   c -= 0x20;
-  for (char row = 0; row < 8; row++) {
+  for (char row = 0; row < 12; row++) {
     unsigned short  rowBits = font_8x12[c][row];
-    for (char col = 0; col < 12; col++) {
+    for (char col = 0; col < 8; col++) {
       unsigned short colMask = 1 << (7 - col);
       putchar( (rowBits & colMask) ? '*' : ' ');
     }
